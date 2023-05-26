896adcdeb31f1bbe9c3da0a82992cc64e87f79d2
Fabchirajoul <fabchirajoul@gmail.com>
2023-05-26 17:43:33 +0200

    changes to vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
-import handlebars from 'vite-plugin-handlebars';
 
 import data from "./data.json" assert {type:'json'};
+import handlebars from 'vite-plugin-handlebars';
+import { defineConfig } from 'vite'
 
-export default {
-  base : '/responsive_webpages/',
-    plugins: [handlebars({
-      context : {
-          data
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input : {
+        main: 'index.html',
+        sub1: 'index-bootstrap.html',
+        sub2: 'index-materialize.html',
       }
-    })],
-  };
+    },
+  },
+  base : '/responsive_webpages/',
+  plugins: [handlebars({
+    context : {
+      data   
+  }
+})],
+})
